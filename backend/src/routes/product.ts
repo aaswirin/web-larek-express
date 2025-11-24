b831c932fe4a9fab0b3d581e6bbe3960c33dd61f
Andrey Svirin <aaswirin@gmail.com>
2025-11-24 15:41:03 +0300

    Наставник посоветовал убрать авторизацию

--- backend/src/routes/product.ts
+++ backend/src/routes/product.ts
@@ -15,8 +15,13 @@ import auth from '../middlewares/auth';
 const router = Router();
 
 router.get('/', readProducts);
+/* Тесты на GitHub'е не проходят с авторизацией
 router.post('/', auth, validateProduct, createProduct);
 router.patch('/:productId', auth, updateProduct);
 router.delete('/:productId', auth, deleteProduct);
+*/
+router.post('/', validateProduct, createProduct);
+router.patch('/:productId', updateProduct);
+router.delete('/:productId', deleteProduct);
 
 export default router;
